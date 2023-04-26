b9c43c1753f5b44d938a6567ba9abe81d424280d
Sharan Thakur <sharandt19@live.com>
2023-04-26 21:38:52 +0530

    Initial Commit

---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Sharan Thakur on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
