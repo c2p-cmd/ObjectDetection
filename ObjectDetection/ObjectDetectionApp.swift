d70774a0f140089ad442142ecb7bcb0c03725ee4
Sharan Thakur <sharandt19@live.com>
2023-05-20 19:08:38 +0530

    added option to views & preview provider

--- ObjectDetection/ObjectDetectionApp.swift
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -11,8 +11,31 @@ import SwiftUI
 struct ObjectDetectionApp: App {
     var body: some Scene {
         WindowGroup {
-            // ContentView()
-             ContentView_ImagePicker()
+            ChooseView()
         }
+    }
+}
+
+struct ChooseView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Go To View")
+                    .font(.largeTitle)
+                    .monospaced()
+                NavigationLink("Realtime", destination: {
+                    ContentView()
+                }).font(.title).monospaced()
+                NavigationLink("Non-Realtime", destination: {
+                    ContentView_ImagePicker()
+                }).font(.title).monospaced()
+            }
+        }
+    }
+}
+
+struct App_Previews: PreviewProvider {
+    static var previews: some View {
+        ChooseView()
     }
 }
